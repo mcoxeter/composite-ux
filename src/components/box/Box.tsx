8ff164f8a3770f63f93ecbe7b8533dcde1303576
Michael Coxeter <mcoxeter@gamil.com>
2021-02-21 06:40:23 +0100

    Added box

---
+++ src/components/box/Box.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import styles from './Box.module.scss';
+export interface BoxProps {
+    width?: string;
+    height?: string;
+    colorToken: ColorTokenNames;
+
+}
+
+export const Box: React.FC<BoxProps> = () => {
+    return (<div className={styles['component']} style={{width: "100px", height: "100px", backgroundColor: "red" }}>Hello</div>);
+}
+
+export type ColorTokenNames = 
+    'page-base' |
+    'action-primary-default' |
+    'action-disabled' |
+    'action-primary-active' |
+    'action-primary-hover' |
+    'action-primary-focused'
--- src/playroom/components/index.ts
+++ src/playroom/components/index.ts
@@ -1,1 +1,2 @@
 export { PrimaryButton } from '../../components/primary-button/PrimaryButton';
+export { Box } from '../../components/box/Box';
